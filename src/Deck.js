dd5f3f83febbf65ce0b27e9f37e676abcf64faef
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-01 11:12:46 -0700

    added onSwipeComplete callback

--- src/Deck.js
+++ src/Deck.js
@@ -9,6 +9,7 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
   constructor(props) {
@@ -22,9 +23,9 @@ class Deck extends Component {
       },
       onPanResponderRelease: () => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
@@ -35,6 +36,18 @@ class Deck extends Component {
     this.state = { panResponder, position };
   }
 
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete() {
+
+  }
+  
   resetPosition() {
     Animated.spring(this.state.position, {
       toValue: { x: 0, y: 0 }
