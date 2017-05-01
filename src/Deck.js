a0cd91670bc2fee072079daf2a3e1ae5503e7a3f
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-01 10:10:16 -0700

    added helper function for rotation

--- src/Deck.js
+++ src/Deck.js
@@ -10,18 +10,43 @@ class Deck extends Component {
   constructor(props) {
     super(props);
 
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => {},
-      onPanResponderMove: () => {},
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
       onPanResponderRelease: () => {}
     });
 
-    this.state = { panResponder };
+    this.state = { panResponder, position };
+  }
 
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+
+    return {
+      position.getLayout(),
+      transform: [{ rotate }]
+    };
   }
 
   renderCards() {
-    return this.props.data.map( item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        <Animated.View
+          key={item.id}
+          style={this.getCardStyle()}
+          {...this.state.panResponder.panHandlers}
+        >
+          {this.props.renderCard(item)}
+        </Animated.View>
+      }
+
       return this.props.renderCard(item);
     });
   }
