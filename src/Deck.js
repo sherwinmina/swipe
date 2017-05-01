4f5a0bffd715d06763a7564dc6769654a6bb431f
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-01 10:53:53 -0700

    setting hand gesture  release like

--- src/Deck.js
+++ src/Deck.js
@@ -3,8 +3,12 @@ import {
   Text, 
   View,
   Animated ,
-  PanResponder
+  PanResponder,
+  Dimensions
 } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 
 class Deck extends Component {
   constructor(props) {
@@ -16,21 +20,36 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: () => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+
+        } else {
+          this.resetPosition();
+        }
+       
+      }
     });
 
     this.state = { panResponder, position };
   }
 
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
   getCardStyle() {
     const { position } = this.state;
     const rotate = position.x.interpolate({
-      inputRange: [-500, 0, 500],
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
       outputRange: ['-120deg', '0deg', '120deg']
     });
 
     return {
-      position.getLayout(),
+      ...position.getLayout(),
       transform: [{ rotate }]
     };
   }
